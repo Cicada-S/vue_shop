a048d8344c20d872a633549f9cf3fb362d7896f6
Cicada <202586563@qq.com>
2021-12-02 11:50:53 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
 Vue.use(VueRouter)
 
@@ -14,7 +16,17 @@ const routes = [
     component: Login
   }, {
     path: '/home',
-    component: Home
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      }, {
+        path: '/users',
+        component: Users
+      }
+    ]
   }
 ]
 
